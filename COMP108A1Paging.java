41c1babee008f7b736f4ae84bb1c84110a198fee
HenryBridges <sghbridg@liverpool.ac.uk>
2021-03-08 18:28:50 +0000

    FIFO done

--- COMP108A1Paging.java
+++ COMP108A1Paging.java
@@ -1,9 +1,10 @@
+
 //
 // Note: You are allowed to add additional methods if you need.
 // Coded by Prudence Wong 2020-12-15
 //
-// Name:
-// Student ID:
+// Name:HenryBridges
+// Student ID:201495180
 //
 // Time Complexity and explanation: You can use the following variables for easier reference.
 // n denotes the number of requests, p denotes the size of the cache
@@ -18,8 +19,11 @@
 // evictLFD():
 //
 
-class COMP108A1Paging {
 
+class COMP108A1Paging {
+	public static int head;
+	public static int tail;
+	public static int qSize;
 
 	// no eviction
 	// Aim: 
@@ -40,10 +44,12 @@ class COMP108A1Paging {
 				}
 			}
 			if(hit == true){
-				output.hitPattern += "h"; // if it exists than it is a hit.
+				output.hitPattern += "h";// if it exists than it is a hit.
+				output.hitCount += 1;
 			}
 			else{
 				output.hitPattern += "m"; // if not then it is a miss.
+				output.missCount += 1;
 			}
 		}
 		return output;
@@ -58,7 +64,31 @@ class COMP108A1Paging {
 	// rArray is an array containing the requested sequence with rSize entries
 	static COMP108A1Output evictFIFO(int[] cArray, int cSize, int[] rArray, int rSize) {
 		COMP108A1Output output = new COMP108A1Output();
-
+		head = 0;
+		qSize = cSize - 1;
+		tail = (qSize + 1);
+		boolean hit = false; // used as a flag to indicate if the element exists in the other array.
+		for(int i = 0; i < rSize ; i++) { // loops through each index of rArray.
+			hit = false; // reset the flag to false after each run through or else will always stay true after its true once.
+			for (int j = 0; j < cSize; j++) { // loops through each index of cArray.
+				if (rArray[i] == cArray[j]) {
+					hit = true;
+					break; // break here as don't need to check it exists more than once.
+				}
+			}
+			if(hit == true){ // Nothing needs to happen as it is already in cache.
+				output.hitPattern += "h";
+				output.hitCount += 1;
+			}
+			else{ // dequeue (FIFO) and insert the value.
+				output.hitPattern += "m";
+				output.missCount += 1;
+				// Dequeue
+				dequeue(cArray);
+				// Enqueue
+				enqueue(rArray[i], cArray);
+			}
+		}
 		return output;
 	}
 
@@ -71,6 +101,7 @@ class COMP108A1Paging {
 	// rArray is an array containing the request sequence with rSize entries
 	static COMP108A1Output evictLFU(int[] cArray, int cSize, int[] rArray, int rSize) {
 		COMP108A1Output output = new COMP108A1Output();
+
 
 		return output;
 	}
@@ -86,6 +117,31 @@ class COMP108A1Paging {
 		COMP108A1Output output = new COMP108A1Output();
 
 		return output;
+	}
+
+
+
+	// Implement a queue with a circular array system. Enqueue adds a value to old tail position.
+	// Increments the tail position with the circular system.
+	static void enqueue(int value, int[] array){
+		if(array.length == tail){
+			System.out.println("Queue is Full");
+		}
+		else{
+			array[tail] = value;
+			tail = (tail + 1) % qSize;
+		}
+	}
+	// Dequeue sets the front of the queue value to 0/null so it can be used and increments the head along by one
+	// in the circular queue system.
+	static void dequeue(int[] array){
+		if(tail == head){
+			System.out.println("Queue is empty");
+		}
+		else{
+			array[head] = 0;
+			head = (head + 1) % qSize;
+		}
 	}
 
 }
